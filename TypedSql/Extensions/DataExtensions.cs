4e17941004ced591ecb54c7d292617875cbe01ee
andersnm <andersnm@users.noreply.github.com>
2019-11-30 16:02:28 +0100

    Simplify materializer

--- TypedSql/Extensions/DataExtensions.cs
+++ TypedSql/Extensions/DataExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -20,7 +21,7 @@ namespace TypedSql
         static bool IsScalarType(Type type)
         {
             var typeInfo = type.GetTypeInfo();
-            return typeInfo.IsPrimitive || type == typeof(string) || type == typeof(DateTime);
+            return typeInfo.IsPrimitive || type == typeof(string) || type == typeof(DateTime) || type == typeof(decimal) || type == typeof(byte[]) || IsEnumType(type);
         }
 
         static bool IsNullable(Type type)
@@ -48,80 +49,19 @@ namespace TypedSql
 
         static T ReadObject<T>(IDataRecord reader, List<SqlMember> selectMembers)
         {
-            var type = typeof(T);
-            var typeInfo = typeof(T).GetTypeInfo();
-            var constructors = typeInfo.GetConstructors();
-            var properties = typeInfo.GetProperties();
+            var propertyType = typeof(T);
+            var typeInfo = propertyType.GetTypeInfo();
 
-            if (IsScalarType(type))
+            if (IsScalarType(propertyType))
             {
-                if (reader.FieldCount != 1)
-                {
-                    throw new InvalidOperationException("Scalar ReadObject<T> has more than one result");
-                }
-
-                return (T)Convert.ChangeType(reader.GetValue(0), type);
+                return (T)ReadSimpleValue(reader, propertyType, 0, out var _);
             }
-            else if (IsNullable(type) && IsScalarType(typeInfo.GenericTypeArguments[0]))
+            else if (IsNullable(propertyType) && IsScalarType(typeInfo.GenericTypeArguments[0]))
             {
-                if (reader.FieldCount != 1)
-                {
-                    throw new InvalidOperationException("Scalar nullable ReadObject<T> has more than one result");
-                }
-
-                object value = reader.GetValue(0);
-                if (value == null || value == DBNull.Value)
-                    return default(T);
-                return (T)Activator.CreateInstance(type, value);
-            }
-            else if (IsAnonymousType(type) && constructors.Length == 1) {
-                var constructor = constructors[0];
-                var parameters = constructor.GetParameters();
-                if (parameters.Length == properties.Length)
-                {
-                    return ReadRowConstructor<T>(constructor, reader, selectMembers);
-                }
+                return (T)ReadSimpleValue(reader, propertyType, 0, out var _);
             }
 
-            // Otherwise choose the constructor without arguments
-            foreach (var constructor in constructors)
-            {
-                var parameters = constructor.GetParameters();
-                if (parameters.Length == 0)
-                {
-                    return ReadRowObject<T>(constructor, reader, selectMembers);
-                }
-            }
-
-            throw new InvalidOperationException("Could not find a constructor for " + typeof(T));
-        }
-
-        static T ReadRowConstructor<T>(ConstructorInfo constructor, IDataRecord reader, List<SqlMember> selectMembers)
-        {
-            var ordinal = 0;
-            var parameters = new List<object>();
-            foreach (var member in selectMembers)
-            {
-                var value = GetValue(reader, member, ordinal, out var usedOrdinals);
-                parameters.Add(value);
-                ordinal += usedOrdinals;
-            }
-
-            return (T)constructor.Invoke(parameters.ToArray());
-        }
-
-        static T ReadRowObject<T>(ConstructorInfo constructor, IDataRecord reader, List<SqlMember> selectMembers)
-        {
-            var ordinal = 0;
-            var row = (T)constructor.Invoke(new object[0]);
-            foreach (var member in selectMembers)
-            {
-                var value = GetValue(reader, member, ordinal, out var usedOrdinals);
-                member.MemberInfo.SetValue(row, value);
-                ordinal += usedOrdinals;
-            }
-
-            return row;
+            return (T)ReadComplexValue(reader, propertyType, selectMembers, 0, out var _);
         }
 
         static bool IsNullObject(IDataRecord reader, List<SqlMember> members, int ordinal, out int usedOrdinals)
@@ -155,59 +95,61 @@ namespace TypedSql
             return isNullObject;
         }
 
-        static object GetValue(IDataRecord reader, SqlMember member, int ordinal, out int usedOrdinals)
+        static object ReadComplexValue(IDataRecord reader, Type propertyType, List<SqlMember> members, int ordinal, out int usedOrdinals)
         {
-            var propertyType = member.MemberInfo.PropertyType;
-
-            // NOTE: Cannot tell the difference between a null object and an object where all fields are null!
-            // Assuming null object if all fields are null
-            if (member is SqlExpressionMember memberExpr && memberExpr.Expression is SqlTableExpression tableExpression)
+            if (IsNullObject(reader, members, ordinal, out int nullOrdinals))
             {
-                if (IsNullObject(reader, tableExpression.TableResult.Members, ordinal, out int nullOrdinals))
-                {
-                    usedOrdinals = nullOrdinals;
-                    return null;
-                }
-
-                if (IsAnonymousType(propertyType))
-                {
-                    var constructorArguments = new List<object>();
-                    usedOrdinals = 0;
-                    foreach (var objectMember in tableExpression.TableResult.Members)
-                    {
-                        var memberValue = GetValue(reader, objectMember, ordinal, out var usedMemberOrdinals);
-                        constructorArguments.Add(memberValue);
-                        usedOrdinals += usedMemberOrdinals;
-                        ordinal += usedMemberOrdinals;
-                    }
-
-                    var obj = Activator.CreateInstance(propertyType, constructorArguments.ToArray());
-                    return obj;
-                }
-                else
-                {
-
-                    var obj = Activator.CreateInstance(propertyType);
-                    usedOrdinals = 0;
-                    foreach (var objectMember in tableExpression.TableResult.Members)
-                    {
-                        var memberValue = GetValue(reader, objectMember, ordinal, out var usedMemberOrdinals);
-                        objectMember.MemberInfo.SetValue(obj, memberValue);
-                        usedOrdinals += usedMemberOrdinals;
-                        ordinal += usedMemberOrdinals;
-                    }
-
-                    return obj;
-                }
+                usedOrdinals = nullOrdinals;
+                return null;
             }
 
+            if (IsAnonymousType(propertyType))
+            {
+                var constructorArguments = new List<object>();
+                usedOrdinals = 0;
+
+                foreach (var objectMember in members)
+                {
+                    var memberValue = ReadMemberValue(reader, objectMember, ordinal, out var usedMemberOrdinals);
+                    constructorArguments.Add(memberValue);
+                    usedOrdinals += usedMemberOrdinals;
+                    ordinal += usedMemberOrdinals;
+                }
+
+                var obj = Activator.CreateInstance(propertyType, constructorArguments.ToArray());
+                return obj;
+            }
+            else
+            {
+                var obj = Activator.CreateInstance(propertyType);
+                usedOrdinals = 0;
+                foreach (var objectMember in members)
+                {
+                    var memberValue = ReadMemberValue(reader, objectMember, ordinal, out var usedMemberOrdinals);
+                    objectMember.MemberInfo.SetValue(obj, memberValue);
+                    usedOrdinals += usedMemberOrdinals;
+                    ordinal += usedMemberOrdinals;
+                }
+
+                return obj;
+            }
+        }
+
+        static object ReadSimpleValue(IDataRecord reader, Type propertyType, int ordinal, out int usedOrdinals)
+        {
+            var typeInfo = propertyType.GetTypeInfo();
             var isNull = reader.IsDBNull(ordinal);
             var value = isNull ? null : reader.GetValue(ordinal);
-            var nullable = IsNullable(propertyType);
 
-            if (nullable)
+            if (IsScalarType(propertyType))
             {
                 usedOrdinals = 1;
+                return ChangeTypeOrEnum(value, propertyType);
+            }
+            else if (IsNullable(propertyType) && IsScalarType(typeInfo.GenericTypeArguments[0]))
+            {
+                usedOrdinals = 1;
+
                 if (value == null || value == DBNull.Value)
                 {
                     return Activator.CreateInstance(propertyType, null);
@@ -216,11 +158,18 @@ namespace TypedSql
                 var underlyingValue = ChangeTypeOrEnum(value, Nullable.GetUnderlyingType(propertyType));
                 return Activator.CreateInstance(propertyType, underlyingValue);
             }
-            else
+
+            throw new InvalidOperationException("Expected scalar in ReadSimpleValue");
+        }
+
+        static object ReadMemberValue(IDataRecord reader, SqlMember member, int ordinal, out int usedOrdinals)
+        {
+            if (member is SqlExpressionMember memberExpr && memberExpr.Expression is SqlTableExpression tableExpression)
             {
-                usedOrdinals = 1;
-                return ChangeTypeOrEnum(value, propertyType);
+                return ReadComplexValue(reader, member.MemberInfo.PropertyType, tableExpression.TableResult.Members, ordinal, out usedOrdinals);
             }
+
+            return ReadSimpleValue(reader, member.MemberInfo.PropertyType, ordinal, out usedOrdinals);
         }
 
         static object ChangeTypeOrEnum(object value, Type conversionType)
@@ -232,7 +181,7 @@ namespace TypedSql
             else
             {
                 // NOTE: if this throws, check for left joins not casting to nullable
-                return Convert.ChangeType(value, conversionType);
+                return Convert.ChangeType(value, conversionType, CultureInfo.InvariantCulture);
             }
         }
     }
